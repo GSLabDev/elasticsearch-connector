f06fcb3251e1c0c16482be8b80cd2eb3560e5186
Niranjan Joshi <niranjan.joshi@gslab.com>
2018-08-24 13:51:24 +0530

    Moved querytype package from api to internal.

--- src/main/java/org/mule/extension/elastic/internal/querytype/BaseMatchPhraseQuery.java
+++ src/main/java/org/mule/extension/elastic/internal/querytype/BaseMatchPhraseQuery.java
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2003-2017, Great Software Laboratory Pvt. Ltd. The software in this package is published under the terms of the Commercial Free Software license V.1, a copy of which has been included with this distribution in the LICENSE.md file.
  */
-package org.mule.extension.elastic.api.querytype;
+package org.mule.extension.elastic.internal.querytype;
 
 import org.mule.runtime.extension.api.annotation.param.Optional;
 import org.mule.runtime.extension.api.annotation.param.Parameter;
