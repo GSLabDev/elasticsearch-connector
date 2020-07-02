99833ff4493aca31f04a8682d19454823d4f43aa
Sneha Modi <sneha.modi@gslab.com>
2020-07-02 16:09:02 +0530

    Removing unused responseConsumer method

--- src/main/java/com/mulesoft/connectors/elasticsearch/internal/operations/ElasticsearchOperations.java
+++ src/main/java/com/mulesoft/connectors/elasticsearch/internal/operations/ElasticsearchOperations.java
@@ -1,10 +1,6 @@
 package com.mulesoft.connectors.elasticsearch.internal.operations;
 
-import static org.mule.runtime.api.metadata.MediaType.APPLICATION_JSON;
-
 import org.mule.runtime.extension.api.annotation.Ignore;
-import org.mule.runtime.extension.api.runtime.operation.Result;
-import org.mule.runtime.extension.api.runtime.process.CompletionCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,27 +18,6 @@ public class ElasticsearchOperations {
 
     private static final Logger logger = LoggerFactory.getLogger(ElasticsearchOperations.class);
     static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-
-    @Ignore
-    protected <P, A> void responseConsumer(P response, CompletionCallback<P, A> callback) {
-        String jsonResponse = null;
-        try {
-            jsonResponse = OBJECT_MAPPER.writeValueAsString(response);
-        } catch (JsonProcessingException e) {
-            logger.error(e.getMessage());
-            responseConsumer(ElasticsearchErrorTypes.EXECUTION, e, callback);
-        }
-
-        Result.Builder resultBuilder = Result.builder().output(jsonResponse).mediaType(APPLICATION_JSON);
-
-        callback.success(resultBuilder.build());
-
-    }
-
-    @Ignore
-    protected <P, A> void responseConsumer(ElasticsearchErrorTypes error, Exception e, CompletionCallback<P, A> callback) {
-        callback.error(new ElasticsearchException(error, e));
-    }
     
     @Ignore
     protected <P> String getJsonResponse(P response) {
